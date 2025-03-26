1c3fb93ff14f2504def16e6af48810e14e815e8a
Ivan Yeh <ivan.hsuu@gmail.com>
2025-03-26 22:10:35 +1100

    update prompt

--- simple-evals/sampler/llama.py
+++ simple-evals/sampler/llama.py
@@ -33,7 +33,7 @@ def get_response(query, message_history=[], max_tokens=2048, temperature=0.6, to
 def response_from_msg_list(msg_list: list[dict], message_history=[], max_tokens=2048, temperature=0.6, top_p=0.9, confidence=False):
     if confidence:
         for x in range(len(msg_list)):
-            msg_list[x]["content"] += " At the end of your response, add double forward slashes followed by your confidence in percentage in the correctness of your response, percentage only without explanation."
+            msg_list[x]["content"] += " If you dont't know, say I don't know. At the end of your response, add your confidence in percentage in the correctness of your response in brackets, percentage only."
     user_prompt = msg_list
     print("message list:", user_prompt)
     prompt = pipeline.tokenizer.apply_chat_template(
