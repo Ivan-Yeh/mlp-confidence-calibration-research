added deep seek llama and logprobs saving, fixed verbal gpqa answer key mismatch

simple-evals/sampler/other_samplers.py
@@ -70,8 +70,10 @@ class HFSampler(SamplerBase):
                         top_p =0.9,
                         pad_token_id=self.tokeniser.eos_token_id
                     )
-                # return response_text and logprob based confidence
-                return "".join(self.tokeniser.batch_decode(outputs.sequences[:, inputs["input_ids"].shape[1]:], skip_special_tokens=True)), float(np.exp(self.model.compute_transition_scores(outputs.sequences, outputs.scores, normalize_logits=True).numpy(force=True)).mean())
+                # return response_text, logprob based confidence and log_probs
+                return ("".join(self.tokeniser.batch_decode(outputs.sequences[:, inputs["input_ids"].shape[1]:], skip_special_tokens=True)), 
+                        float(np.exp(self.model.compute_transition_scores(outputs.sequences, outputs.scores, normalize_logits=True).numpy(force=True)).mean()), 
+                        self.model.compute_transition_scores(outputs.sequences, outputs.scores, normalize_logits=True).numpy(force=True))
 
             # NOTE: BadRequestError is triggered once for MMMU, please uncomment if you are reruning MMMU
             except Exception as e:
@@ -133,8 +135,8 @@ class DBSampler(SamplerBase):
                 client = OpenAI(api_key=self.api_key, base_url=(self.model_url[0] if isinstance(self.model_url, tuple) else self.model_url))
                 chat_completion = client.chat.completions.create(messages=message_list, model=self.model_name, max_tokens=self.max_tokens, logprobs=True)
 
-                # return response_text and logprob based confidence
-                return chat_completion.choices[0].message.content, float(np.exp(np.array([t.logprob for t in chat_completion.choices[0].logprobs.content])).mean())
+                # return response_text, logprob based confidence and log_probs
+                return chat_completion.choices[0].message.content, float(np.exp(np.array([t.logprob for t in chat_completion.choices[0].logprobs.content])).mean()), [t.logprob for t in chat_completion.choices[0].logprobs.content]
             # NOTE: BadRequestError is triggered once for MMMU, please uncomment if you are reruning MMMU
             except Exception as e:
                 exception_backoff = 2**trial  # expontial back off
@@ -195,10 +197,10 @@ class TogetherSampler(SamplerBase):
         while True:
             try:
                 client = Together(api_key=self.api_key)
-                chat_completion = client.chat.completions.create(messages=message_list, model=self.model_id, max_tokens=self.max_tokens, logprobs=1)
+                chat_completion = client.chat.completions.create(messages=message_list, model=(self.model_id[0] if isinstance(self.model_id, tuple) else self.model_id), max_tokens=self.max_tokens, logprobs=1)
 
-                # return response_text and logprob based confidence
-                return chat_completion.choices[0].message.content, float(np.exp(chat_completion.choices[0].logprobs.token_logprobs).mean())
+                # return response_text, logprob based confidence and log_probs
+                return chat_completion.choices[0].message.content, float(np.exp(chat_completion.choices[0].logprobs.token_logprobs).mean()), chat_completion.choices[0].logprobs.token_logprobs
             # NOTE: BadRequestError is triggered once for MMMU, please uncomment if you are reruning MMMU
             except Exception as e:
                 exception_backoff = 2**trial  # expontial back off

simple-evals/simple_evals_ext.py
@@ -121,6 +121,7 @@ def main():
         # ),
         # HF models: None by default; only constructed when chosen
         "mistralai/Mixtral-8x7B-Instruct-v0.1": TogetherSampler(api_key=TOGETHER_AI_TOKEN, model_id="mistralai/Mixtral-8x7B-Instruct-v0.1", model_name="mistralai/Mixtral-8x7B-Instruct-v0.1", max_tokens=128),
+        "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8": TogetherSampler(api_key=TOGETHER_AI_TOKEN, model_id="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8", model_name="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8", max_tokens=2048),
         "databricks-meta-llama-3-3-70b-instruct": DBSampler(api_key=DATABRICKS_TOKEN, model_url="https://dbc-b04afa5d-8e3e.cloud.databricks.com/serving-endpoints", model_name="databricks-meta-llama-3-3-70b-instruct", max_tokens=128),
         "databricks-claude-3-7-sonnet": DBSampler(api_key=DATABRICKS_TOKEN, model_url="https://dbc-b04afa5d-8e3e.cloud.databricks.com/serving-endpoints", model_name="databricks-claude-3-7-sonnet", max_tokens=128),
         "meta-llama/Llama-3.2-3B-Instruct": None,
@@ -184,11 +185,16 @@ def main():
     # equality_checker = ChatCompletionSampler(model="gpt-4-turbo-preview")
     # ^^^ used for fuzzy matching, just for math
 
-    local_dir = snapshot_download(repo_id="meta-llama/Llama-3.2-3B-Instruct")
-    model = LlamaForCausalLM.from_pretrained(local_dir, torch_dtype=torch.float16, low_cpu_mem_usage=True)
-    tokeniser = AutoTokenizer.from_pretrained(local_dir)
-    grading_sampler = HFSampler(model=model, tokeniser=tokeniser, model_name=args.model, max_new_tokens=256)
-    equality_checker = HFSampler(model=model, tokeniser=tokeniser, model_name=args.model, max_new_tokens=256)
+    # local_dir = snapshot_download(repo_id="meta-llama/Llama-3.2-3B-Instruct")
+    # model = LlamaForCausalLM.from_pretrained(local_dir, torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    # tokeniser = AutoTokenizer.from_pretrained(local_dir)
+    # grading_sampler = HFSampler(model=model, tokeniser=tokeniser, model_name=args.model, max_new_tokens=256)
+    # equality_checker = HFSampler(model=model, tokeniser=tokeniser, model_name=args.model, max_new_tokens=256)
+    # ^^^ use local LLAMA
+
+    grading_sampler = TogetherSampler(api_key=TOGETHER_AI_TOKEN, model_id="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8", model_name="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8", max_tokens=2048)
+    equality_checker = TogetherSampler(api_key=TOGETHER_AI_TOKEN, model_id="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8", model_name="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8", max_tokens=2048)
+    # ^^^ use Together AI
 
     def get_evals(eval_name, debug_mode):
         num_examples = (